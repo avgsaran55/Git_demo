f041dc6a38d3684771298be517a90f89a767c681
avgsaran55 <sarankumar282002@gmail.com>
2026-04-02 18:47:26 +0530

    Revert "Add second console"
    This reverts commit afae7d4f183c432089aceee59a8b089787e290c1.

--- Git_demo/Program.cs
+++ Git_demo/Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! demo");
 Console.WriteLine("Thank to the world! demo");
-Console.WriteLine("Form the art! demo");
