afae7d4f183c432089aceee59a8b089787e290c1
avgsaran55 <165462777+avgsaran55@users.noreply.github.com>
2026-04-02 18:44:43 +0530

    Add second console

--- Git_demo/Program.cs
+++ Git_demo/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! demo");
 Console.WriteLine("Thank to the world! demo");
+Console.WriteLine("Form the art! demo");
